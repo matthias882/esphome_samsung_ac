75c5895533b5af1b8e5b253beba6e6cf6091ca24
matthias882 <30553262+matthias882@users.noreply.github.com>
2026-01-09 19:10:03 +0100

    Update protocol_non_nasa.cpp

--- components/samsung_ac/protocol_non_nasa.cpp
+++ components/samsung_ac/protocol_non_nasa.cpp
@@ -900,7 +900,7 @@ namespace esphome
                     // We know the outdoor unit is awake due to this request_control message, so we only
                     // need to check that the indoor unit is awake.
                     LOGD("--Daten werden gesendet--");
-                    delay(20);
+                    delay(10);
                     send_requests(target);
                 }
             }
@@ -997,6 +997,7 @@ namespace esphome
         }
     } // namespace samsung_ac
 } // namespace esphome
+
 
 
 
